c443632d75a64b8d21a03d3980021825e83112ba
Robson Fernandes <robfds@gmail.com>
2016-06-29 10:32:10 -0300

    Encrypting the user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,9 @@
 class User < ActiveRecord::Base
   EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
-  validates_presence_of :email, :full_name, :location, :password
-  validates_confirmation_of :password
+  validates_presence_of :email, :full_name, :location
   validates_length_of :bio, minimum: 30, allow_blank: false
   validates_uniqueness_of :email
+  validates_format_of :email, with: EMAIL_REGEXP
 
-  validate :email_format
-
-  private
-  def email_format
-    errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
-  end
+  has_secure_password
 end
