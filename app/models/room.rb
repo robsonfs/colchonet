450d00f8073c6d3c80ca5ae7e9db28f37a477883
Robson Fernandes <robfds@gmail.com>
2016-07-22 10:20:44 -0300

    Add validation to room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ActiveRecord::Base
+  validates_presence_of :title, :location, :description
+  validates_length_of :description, minimum: 30, allow_blank: false
   def complete_name
     "#{title}, #{location}"
   end
