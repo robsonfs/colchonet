d75495c241fdb081ce2fcb643287e53d0827b87e
Robson Fernandes <robfds@gmail.com>
2016-10-30 04:39:40 -0300

    Fixing translation error missing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
 
   def require_authentication
     unless user_signed_in?
-      redirect_to new_user_sessions_path, alert: t('flash.alert.nedds_login')
+      redirect_to new_user_sessions_path, alert: t('flash.alert.needs_login')
     end
   end
 
